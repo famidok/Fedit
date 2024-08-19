0dd954eeb1c77c1207cfca97748e4d92b79963d7
famidok <onder.emir@protonmail.com>
2024-08-20 01:17:21 +0300

    Turn off canonical mode and add display keypresses

--- fedit.c
+++ fedit.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,10 +17,9 @@ void ENABLE_RAW_MODE()
 	atexit(DISABLE_RAW_MODE);
 
 	struct termios raw = orig_termios;	
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-
 }
 
 int main() 
@@ -26,7 +27,16 @@ int main()
 	ENABLE_RAW_MODE();
 
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	{
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		}
+		else
+		{
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }
 
