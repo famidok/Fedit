8736e0c6ba36c15112cfd4f1257fd88471ad77a3
famidok <onder.emir@protonmail.com>
2024-08-20 03:20:12 +0300

    Turn off all output processing and Add miscellaneous flags

--- fedit.c
+++ fedit.c
@@ -17,7 +17,9 @@ void ENABLE_RAW_MODE()
 	atexit(DISABLE_RAW_MODE);
 
 	struct termios raw = orig_termios;	
-	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | IXON | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -31,11 +33,11 @@ int main()
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		}
 		else
 		{
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
 	}
 	return 0;
